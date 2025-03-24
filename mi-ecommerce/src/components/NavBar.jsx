62c2da764fb1147a366faf16936b13f366a413ec
unknown <ezequielvalladares.ezze@gmail.com>
2025-03-23 22:22:20 -0300

    CreaTuLanding1Valladares

--- mi-ecommerce/src/components/NavBar.jsx
+++ mi-ecommerce/src/components/NavBar.jsx
@@ -4,8 +4,12 @@ import CartWidget from "./CartWidget";
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-white shadow-md">
-      <div>
-        <h1 className="text-xl font-bold">Mi Tienda</h1>
+      <div className="navbar-brand">
+        <img
+          src="./assets/comercio-electronico.png"
+          alt="Logo"
+          className="navbar-logo w-24 h-auto" // Ajusta el tamaño del logo aquí
+        />
       </div>
       <ul className="flex space-x-6">
         <li><a href="#" className="text-gray-700 hover:text-blue-500">Inicio</a></li>
